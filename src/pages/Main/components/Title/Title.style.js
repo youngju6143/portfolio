b69ee9d7dd1e27f8aaf02f81c80ec0e2b42d02b6
youngju6143 <youngju6143@naver.com>
2024-10-13 20:44:18 +0900

    ✨ Feat :  Title 구현

---
+++ src/pages/Main/components/Title/Title.style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  h1 { 
+    width: 100%;
+    color: #fff;
+    font-size: 3rem;
+    text-align: center;
+  }
+`
+
