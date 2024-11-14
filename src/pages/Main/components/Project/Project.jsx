5ee6c9fd893674b68de6f53ee157f30e305aad75
youngju6143 <youngju6143@naver.com>
2024-11-15 03:37:53 +0900

    ✨ Feat :  Skill, Project 컴포넌트 생성

---
+++ src/pages/Main/components/Project/Project.jsx
@@ -0,0 +1,14 @@
+import * as S from "../Profile/Profile.style";
+
+function Project() {
+  return (
+    <S.Container>
+      <S.Title>my PROJECT</S.Title>
+      <S.Bottom>
+        <S.Infos></S.Infos>
+      </S.Bottom>
+    </S.Container>
+  );
+}
+
+export default Project;
---
+++ src/pages/Main/components/Skill/Skill.jsx
@@ -0,0 +1,14 @@
+import * as S from "../Profile/Profile.style";
+
+function Skill() {
+  return (
+    <S.Container>
+      <S.Title>my SKILL</S.Title>
+      <S.Bottom>
+        <S.Infos></S.Infos>
+      </S.Bottom>
+    </S.Container>
+  );
+}
+
+export default Skill;
