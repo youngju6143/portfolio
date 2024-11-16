fdea573023a3f537b51eae260efd09de6569cf71
youngju6143 <youngju6143@naver.com>
2024-11-16 11:56:03 +0900

    💄 Design:  Title 수정

--- src/pages/Main/components/Title/Title.style.js
+++ src/pages/Main/components/Title/Title.style.js
@@ -6,12 +6,18 @@ export const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
+  font-family: "GumiRomanceTTF", sans-serif;
 
-  h1 { 
+  h1 {
     width: 100%;
     color: #fff;
     font-size: 3rem;
     text-align: center;
   }
-`
 
+  p {
+    font-size: 2rem;
+    color: #fff;
+  }
+`;
