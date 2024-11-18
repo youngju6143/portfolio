c641b3051dc5206f2ebe6a80028c757769ca5a0e
youngju6143 <youngju6143@naver.com>
2024-11-18 15:58:24 +0900

    📦️ Chore :  포트번호 변경 - 5174

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['sharp']
-  }
-})
+    exclude: ["sharp"],
+  },
+  server: {
+    port: 5174, // B 프로젝트에서는 5174 포트로 설정
+  },
+});
