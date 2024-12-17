450ff881a2f1e668ff5806878fed9aebb88021c8
youngju6143 <youngju6143@naver.com>
2024-12-17 17:27:28 +0900

    ✨ Feat :  프로젝트 이미지 추가

--- src/pages/components/Project/Project.jsx
+++ src/pages/components/Project/Project.jsx
@@ -1,14 +1,21 @@
 import * as S from "../Profile/Profile.style";
 import * as T from "./Project.style";
 
+import writeroom from "./assets/writeroom.jpg";
+import simter from "./assets/simter.jpg";
+
 function Project() {
   return (
     <S.Container>
       <S.Title>my PROJECT</S.Title>
       <S.Bottom>
         <S.Infos isProject={true}>
-          <T.Wrapper></T.Wrapper>
-          <T.Wrapper></T.Wrapper>
+          <T.Wrapper>
+            <img src={writeroom} alt="writeroom" />
+          </T.Wrapper>
+          <T.Wrapper>
+            <img src={simter} alt="writeroom" />
+          </T.Wrapper>
         </S.Infos>
       </S.Bottom>
     </S.Container>
--- src/pages/components/Project/Project.style.js
+++ src/pages/components/Project/Project.style.js
@@ -2,7 +2,15 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   width: 100%;
-  height: 20vh;
+  // height: 20vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
   border: 1px black solid;
+
+  img {
+    width: 30%;
+  }
 `;
